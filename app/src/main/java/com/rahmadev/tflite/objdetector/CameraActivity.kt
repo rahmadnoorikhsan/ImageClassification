3aa8f0e22bfd4d75316a021aeb4f2d312e32902b
rahmadnoorikhsan <rajaikhsan2021@gmail.com>
2024-03-15 20:59:13 +0700

    Initial commit

---
+++ app/src/main/java/com/rahmadev/tflite/objdetector/CameraActivity.kt
@@ -0,0 +1,138 @@
+package com.rahmadev.tflite.objdetector
+
+import android.os.Build
+import android.os.Bundle
+import android.util.Log
+import android.view.WindowInsets
+import android.view.WindowManager
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.Preview
+import androidx.camera.core.resolutionselector.AspectRatioStrategy
+import androidx.camera.core.resolutionselector.ResolutionSelector
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
+import com.rahmadev.tflite.objdetector.databinding.ActivityCameraBinding
+import com.rahmadev.tflite.utils.ImageClassifierHelper
+import org.tensorflow.lite.task.vision.classifier.Classifications
+import java.text.NumberFormat
+import java.util.concurrent.Executors
+
+class CameraActivity : AppCompatActivity() {
+    private lateinit var imageClassifierHelper: ImageClassifierHelper
+    private lateinit var binding: ActivityCameraBinding
+    private var cameraSelector: CameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityCameraBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+
+    public override fun onResume() {
+        super.onResume()
+        hideSystemUI()
+        startCamera()
+    }
+
+    private fun startCamera() {
+        imageClassifierHelper = ImageClassifierHelper(
+            context = this,
+            classifierListener = object : ImageClassifierHelper.ClassifierListener {
+                override fun onError(error: String) {
+                    runOnUiThread {
+                        Toast.makeText(this@CameraActivity, error, Toast.LENGTH_SHORT).show()
+                    }
+                }
+
+                override fun onResult(result: List<Classifications>?, inferenceTime: Long) {
+                    runOnUiThread {
+                        result?.let { it ->
+                            if (it.isNotEmpty() && it[0].categories.isNotEmpty()) {
+                                println(it)
+                                val sortedCategories =
+                                    it[0].categories.sortedByDescending { it?.score }
+                                val displayResult = sortedCategories.joinToString("\n") { category ->
+                                    "${category.label} " + NumberFormat.getPercentInstance()
+                                        .format(category.score).trim()
+                                }
+                                binding.apply {
+                                    tvResult.text = displayResult
+                                    tvInferenceTime.text = "$inferenceTime ms"
+                                }
+                            } else {
+                                binding.apply {
+                                    tvResult.text = "Tidak Terdeteksi"
+                                    tvInferenceTime.text = "Tidak Terdeteksi"
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        )
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+
+        cameraProviderFuture.addListener({
+            val resolutionSelector = ResolutionSelector.Builder()
+                .setAspectRatioStrategy(AspectRatioStrategy.RATIO_16_9_FALLBACK_AUTO_STRATEGY)
+                .build()
+            val imageAnalyzer = ImageAnalysis.Builder()
+                .setResolutionSelector(resolutionSelector)
+                .setTargetRotation(binding.viewFinder.display.rotation)
+                .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                .setOutputImageFormat(ImageAnalysis.OUTPUT_IMAGE_FORMAT_RGBA_8888)
+                .build()
+            imageAnalyzer.setAnalyzer(Executors.newSingleThreadExecutor()) { image ->
+                imageClassifierHelper.classifyImage(image)
+            }
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(binding.viewFinder.surfaceProvider)
+                }
+
+            try {
+                cameraProvider.unbindAll()
+                cameraProvider.bindToLifecycle(
+                    this,
+                    cameraSelector,
+                    preview,
+                    imageAnalyzer
+                )
+
+            } catch (exc: Exception) {
+                Toast.makeText(
+                    this@CameraActivity,
+                    "Gagal memunculkan kamera.",
+                    Toast.LENGTH_SHORT
+                ).show()
+                Log.e(TAG, "startCamera: ${exc.message}")
+            }
+        }, ContextCompat.getMainExecutor(this))
+    }
+
+    private fun hideSystemUI() {
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+        supportActionBar?.hide()
+    }
+
+    companion object {
+        private const val TAG = "CameraActivity"
+        const val EXTRA_CAMERAX_IMAGE = "CameraX Image"
+        const val CAMERAX_RESULT = 200
+    }
+}
---
+++ app/src/main/java/com/rahmadev/tflite/utils/ImageClassifierHelper.kt
@@ -0,0 +1,110 @@
+package com.rahmadev.tflite.utils
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.os.SystemClock
+import android.util.Log
+import android.view.Surface
+import androidx.camera.core.ImageProxy
+import com.rahmadev.tflite.objdetector.R
+import org.tensorflow.lite.DataType
+import org.tensorflow.lite.support.common.ops.CastOp
+import org.tensorflow.lite.support.image.ImageProcessor
+import org.tensorflow.lite.support.image.TensorImage
+import org.tensorflow.lite.support.image.ops.ResizeOp
+import org.tensorflow.lite.task.core.BaseOptions
+import org.tensorflow.lite.task.core.vision.ImageProcessingOptions
+import org.tensorflow.lite.task.vision.classifier.Classifications
+import org.tensorflow.lite.task.vision.classifier.ImageClassifier
+
+class ImageClassifierHelper(
+    private var threshold: Float = 0.1f,
+    private var maxResult: Int = 3,
+    private val modelName: String = "mobilenet_v1.tflite",
+    val context: Context,
+    val classifierListener: ClassifierListener?,
+) {
+    private var imageClassifier: ImageClassifier? = null
+
+    init {
+        setupImageClassifier()
+    }
+
+    private fun setupImageClassifier() {
+        val optionBuilder = ImageClassifier.ImageClassifierOptions.builder()
+            .setScoreThreshold(threshold)
+            .setMaxResults(maxResult)
+        val baseOptionsBuilder = BaseOptions.builder()
+            .setNumThreads(4)
+        optionBuilder.setBaseOptions(baseOptionsBuilder.build())
+
+        try {
+            imageClassifier = ImageClassifier.createFromFileAndOptions(
+                context,
+                modelName,
+                optionBuilder.build()
+            )
+        } catch (e: IllegalStateException) {
+            classifierListener?.onError(context.getString(R.string.image_classifier_failed))
+            Log.e(TAG, e.message.toString())
+        }
+    }
+
+    fun classifyImage(image: ImageProxy) {
+        if (imageClassifier == null) {
+            setupImageClassifier()
+        }
+
+        val imageProcessor = ImageProcessor.Builder()
+            .add(ResizeOp(224, 224, ResizeOp.ResizeMethod.NEAREST_NEIGHBOR))
+            .add(CastOp(DataType.UINT8))
+            .build()
+
+        val tensorImage = imageProcessor.process(TensorImage.fromBitmap(toBitmap(image)))
+
+        val imageProcessingOptions = ImageProcessingOptions.builder()
+            .setOrientation(getOrientationFromRotate(image.imageInfo.rotationDegrees))
+            .build()
+
+        var inferenceTime = SystemClock.uptimeMillis()
+        val results = imageClassifier?.classify(tensorImage, imageProcessingOptions)
+        inferenceTime = SystemClock.uptimeMillis() - inferenceTime
+        classifierListener?.onResult(
+            results,
+            inferenceTime
+        )
+
+    }
+
+    private fun getOrientationFromRotate(rotationDegrees: Int): ImageProcessingOptions.Orientation {
+        return when (rotationDegrees) {
+            Surface.ROTATION_270 -> ImageProcessingOptions.Orientation.BOTTOM_RIGHT
+            Surface.ROTATION_180 -> ImageProcessingOptions.Orientation.RIGHT_BOTTOM
+            Surface.ROTATION_90 -> ImageProcessingOptions.Orientation.TOP_LEFT
+            else -> ImageProcessingOptions.Orientation.RIGHT_TOP
+        }
+    }
+
+    private fun toBitmap(image: ImageProxy): Bitmap {
+        val bitmapBuffer = Bitmap.createBitmap(
+            image.width,
+            image.height,
+            Bitmap.Config.ARGB_8888
+        )
+        image.use { bitmapBuffer.copyPixelsFromBuffer(image.planes[0].buffer) }
+        image.close()
+        return bitmapBuffer
+    }
+
+    interface ClassifierListener {
+        fun onError(error: String)
+        fun onResult(
+            result: List<Classifications>?,
+            inferenceTime: Long,
+        )
+    }
+
+    companion object {
+        private const val TAG = "ImageClassifierHelper"
+    }
+}
